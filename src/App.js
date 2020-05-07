fadb91715138f744c7663f105993fa0b0eb73884
Mit Chan <leminc@uci.edu>
2020-05-07 09:33:55 -0700

    Fix package.json file

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import { Container } from "react-bootstrap";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import './components/Navbar.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+import CustomNavbar from "./components/Navbar";
+import About from "./components/About";
+import Projects from "./components/Projects";
+import CustomFooter from "./components/CustomFooter";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <CustomNavbar />
+        <Container className="page-container">
+          <About />
+          <Projects /> 
+          {/* <CustomFooter /> */}
+        </Container>
+        
+
+        {/* <Footer /> */}
+      </div>
+    );
+  }
+
 }
 
 export default App;
