a6069308a20b773f741152c94b004ea35cbd286b
Mit Chan <leminc@uci.edu>
2020-05-11 15:03:02 -0700

    Updates

---
+++ precache-manifest.a5f5587044ea0b65241815dbd34414d8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0316d998842481c39553d687591f115b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ca8ad9ffb4ed9520420",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "50b9bab3e9e6cd30ae27",
+    "url": "/static/css/main.187a863d.chunk.css"
+  },
+  {
+    "revision": "5ca8ad9ffb4ed9520420",
+    "url": "/static/js/2.fdd1bb69.chunk.js"
+  },
+  {
+    "revision": "4e3f8d4bad1db285aba0fceffdc7b967",
+    "url": "/static/js/2.fdd1bb69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50b9bab3e9e6cd30ae27",
+    "url": "/static/js/main.a9426ce7.chunk.js"
+  },
+  {
+    "revision": "9d7ec453320509f6d66c",
+    "url": "/static/js/runtime-main.42cfb24a.js"
+  },
+  {
+    "revision": "b5b4bc830be4e897bc688a1156d86af2",
+    "url": "/static/media/Chan_Lemin_Mit_Resume.b5b4bc83.pdf"
+  },
+  {
+    "revision": "2cdf769199c91274c108d6a5026222b1",
+    "url": "/static/media/me.2cdf7691.jpg"
+  },
+  {
+    "revision": "c080db21ed2c2981085bc3a85c9e408d",
+    "url": "/static/media/moon-dark.c080db21.png"
+  },
+  {
+    "revision": "3398019c0c8bde279e7496ab6f3ad701",
+    "url": "/static/media/moon-light.3398019c.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/personal-website/precache-manifest.8a64e53f8fef6b75a1d6f83292abfb68.js"
+  "/precache-manifest.a5f5587044ea0b65241815dbd34414d8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/personal-website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
