f2512c917c0e78997889735c00cd009bfedef655
Mit Chan <leminc@uci.edu>
2020-05-11 15:48:16 -0700

    Updates

---
+++ precache-manifest.980e48f973886a17d0f1bf753739442b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce1c7450875812baf1b34b8f7151c322",
+    "url": "/personal-website/index.html"
+  },
+  {
+    "revision": "76a666e442ce8896214a",
+    "url": "/personal-website/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "12a730ce6415bed3a761",
+    "url": "/personal-website/static/css/main.187a863d.chunk.css"
+  },
+  {
+    "revision": "76a666e442ce8896214a",
+    "url": "/personal-website/static/js/2.2346ed3a.chunk.js"
+  },
+  {
+    "revision": "4e3f8d4bad1db285aba0fceffdc7b967",
+    "url": "/personal-website/static/js/2.2346ed3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12a730ce6415bed3a761",
+    "url": "/personal-website/static/js/main.3e680b3a.chunk.js"
+  },
+  {
+    "revision": "8a0435702aa2b6325ff8",
+    "url": "/personal-website/static/js/runtime-main.d49f66af.js"
+  },
+  {
+    "revision": "b5b4bc830be4e897bc688a1156d86af2",
+    "url": "/personal-website/static/media/Chan_Lemin_Mit_Resume.b5b4bc83.pdf"
+  },
+  {
+    "revision": "2cdf769199c91274c108d6a5026222b1",
+    "url": "/personal-website/static/media/me.2cdf7691.jpg"
+  },
+  {
+    "revision": "c080db21ed2c2981085bc3a85c9e408d",
+    "url": "/personal-website/static/media/moon-dark.c080db21.png"
+  },
+  {
+    "revision": "3398019c0c8bde279e7496ab6f3ad701",
+    "url": "/personal-website/static/media/moon-light.3398019c.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a5f5587044ea0b65241815dbd34414d8.js"
+  "/personal-website/precache-manifest.980e48f973886a17d0f1bf753739442b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/personal-website/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
